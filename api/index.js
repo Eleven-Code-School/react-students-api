ed5cb3b58377fd9ad6db24c6a33ded889e1fb7f0
Juan Macias <info@juanmacias.es>
2025-08-25 18:46:12 +0200

    chore(deploy): update vercel config

--- api/index.js
+++ api/index.js
@@ -1,11 +1,8 @@
-// import serverless from 'serverless-http';
+import 'dotenv/config';
 import app from './app.js';
-
-// export default serverless(app);
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`[dev] API listening on http://localhost:${PORT}/api`);
 });
-
